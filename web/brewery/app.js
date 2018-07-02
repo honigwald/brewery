6c174f9eb1c5a4d93b7c900477242ad5c33058eb
Cristofer Soler <cristofer.soler@yahoo.de>
2018-07-02 18:33:38 +0200

    new stuff in the Frontend

--- web/brewery/app.js
+++ web/brewery/app.js
@@ -6,47 +6,57 @@ var bodyParser = require('body-parser');
 
 const fs = require('fs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
+app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-var recipeList = [];
-//app.get("/", (req, res) => { res.render("recipe", { recName: req.body.recipeName }); });
-app.get('/', function (req, res) {
-
+//get all recipes indexpage
+app.get('/index', function (req, res) {
+    var recipeList = [];
     fs.readdir('recipes/', (err, files) => {
         files.forEach(file => {
+            recipeList.push(file)
             console.log(file);
-            recipeList.add(file);
-            //res.render("index", { recName: req.body.recipeName });
-            /*var obj = JSON.parse(file);
-            var keys = Object.keys(obj);
-            for (var i = 0; i < keys.length; i++) {
-                console.log(obj[keys[i]]);
-            }*/
-            //res.send(req.body.recipeName);
-
         });
+        res.render('index', { files: recipeList });
     });
+
 });
 
+//get all recipes
+app.get('/recipes', function (req, res) {
+    var recipeList = [];
+    fs.readdir('recipes/', (err, files) => {
+        files.forEach(file => {
+            recipeList.push(file)
+            console.log(file);
+            });
+        res.render('recipes', { files: recipeList });
+    });
+
+});
+
+//detail recipes
+app.get('/recipe', function (req, res) {
+    console.log(req.query.name);
+    var contents = fs.readFileSync('recipes/' + req.query.name);
+    var jsonContent = JSON.parse(contents);
+    res.render('recipe', {data : jsonContent});
+
+});
+
+
+//create Recipe
 app.post('/addRecipe', function(req, res) {
-
-
-    //var obj = { step1 :{name : req.body.schritt1Name, temp : req.body.schritt1Temp, time : req.body.schritt1Zeit}, step2: {name : req.body.schritt2Name, temp : req.body.schritt2Temp, time : req.body.schritt2Zeit}};
-    //obj.step3 = {name : req.body.schritt3Name, temp : req.body.schritt3Temp, time : req.body.schritt3Zeit};
-    //var jsonObj = JSON.stringify(obj);
 
 
     var jsonObj2 = JSON.stringify(req.body);
@@ -55,15 +65,10 @@ app.post('/addRecipe', function(req, res) {
             res.send(err);
         }
         else {
-
-
-            res.redirect('/recipe.html');
+            res.redirect('/recipes');
         }
         console.log("The file was saved!");
-
     });
-
-
 });
 
 
